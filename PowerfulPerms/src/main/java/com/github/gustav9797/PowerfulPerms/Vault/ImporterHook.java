7d85d4ef36c55af6f99bee496c5539e6c63b3929
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-02-13 22:35:44 +0100

    Updated the ImporterHook
    Updated the ImporterHook to support the new API of Importer.

--- PowerfulPerms/src/main/java/com/github/gustav9797/PowerfulPerms/Vault/ImporterHook.java
+++ PowerfulPerms/src/main/java/com/github/gustav9797/PowerfulPerms/Vault/ImporterHook.java
@@ -5,6 +5,10 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import eu.taigacraft.importer.ImporterCallback;
+import eu.taigacraft.importer.permissions.PermissionsImporter;
+
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import com.github.gustav9797.PowerfulPerms.common.PermissionManagerBase;
@@ -15,123 +19,133 @@ import com.github.gustav9797.PowerfulPermsAPI.PermissionManager;
 import com.github.gustav9797.PowerfulPermsAPI.PowerfulPermsPlugin;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import eu.taigacraft.importer.permissions.PermissionsImporter;
-
-public class ImporterHook implements eu.taigacraft.importer.permissions.PermissionsImporter {
+public class ImporterHook implements PermissionsImporter {
     
-    PowerfulPermsPlugin plugin;
-    PermissionManager permissionManager;
+    private PowerfulPermsPlugin plugin;
+    private PermissionManager permissionManager;
 
     public void hook(PowerfulPermsPlugin plugin) {
         this.plugin = plugin;
         permissionManager = plugin.getPermissionManager();
-        PermissionsImporter.register("PowerfulPerms", this);
+        PermissionsImporter.register(Bukkit.getPluginManager().getPlugin("PowerfulPerms"), this);
     }
 
-    // Prefix
 
     @Override
-    public String getPrefix(OfflinePlayer player) {
-        return getPrefix(player, null, null);
-    }
-
-    @Override
-    public String getPrefix(OfflinePlayer player, String worldname) {
-        return getPrefix(player);
-    }
-
-    @Override
-    public String getPrefix(OfflinePlayer player, String worldname, String ladder) {
-        ListenableFuture<String> second = permissionManager.getPlayerPrefix(player.getUniqueId(), ladder);
-        try {
-            return second.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    // Suffix
-
-    @Override
-    public String getSuffix(OfflinePlayer player) {
-        return getSuffix(player, null, null);
-    }
-
-    @Override
-    public String getSuffix(OfflinePlayer player, String worldname) {
-        return getSuffix(player);
-    }
-
-    @Override
-    public String getSuffix(OfflinePlayer player, String worldname, String ladder) {
-        ListenableFuture<String> second = permissionManager.getPlayerSuffix(player.getUniqueId(), ladder);
-        try {
-            return second.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public String getRank(OfflinePlayer player) {
-        ListenableFuture<Group> second = permissionManager.getPlayerPrimaryGroup(player.getUniqueId());
+    public void getRank(OfflinePlayer offlinePlayer, ImporterCallback<String> importerCallback) {
+        ListenableFuture<Group> second = permissionManager.getPlayerPrimaryGroup(offlinePlayer.getUniqueId());
         try {
             Group group = second.get();
             if (group != null)
-                return group.getName();
+                importerCallback.call(group.getName());
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
+            importerCallback.call(null);
         }
-        return null;
     }
 
     @Override
-    public Boolean hasPermission(OfflinePlayer player, String permission) {
-        return hasPermission(player, permission, "");
-    }
-
-    @Override
-    public Boolean hasPermission(OfflinePlayer player, String permission, String world) {
-        ListenableFuture<Boolean> second = permissionManager.playerHasPermission(player.getUniqueId(), permission, world, "");
+    public void getRanks(OfflinePlayer offlinePlayer, ImporterCallback<List<String>> importerCallback) {
         try {
-            return second.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public List<String> getRanks(OfflinePlayer player) {
-        try {
-            ListenableFuture<LinkedHashMap<String, List<CachedGroup>>> second = permissionManager.getPlayerCurrentGroups(player.getUniqueId());
+            ListenableFuture<LinkedHashMap<String, List<CachedGroup>>> second = permissionManager.getPlayerCurrentGroups(offlinePlayer.getUniqueId());
             LinkedHashMap<String, List<CachedGroup>> currentGroups = second.get();
             List<CachedGroup> cachedGroups = PermissionPlayerBase.getCachedGroups(PermissionManagerBase.serverName, currentGroups);
             List<Group> groups = PermissionPlayerBase.getGroups(cachedGroups, plugin);
             List<String> groupNames = new ArrayList<>();
             for (Group group : groups)
                 groupNames.add(group.getName());
-            return groupNames;
+            importerCallback.call(groupNames);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
+            importerCallback.call(new ArrayList<>());
         }
-        return new ArrayList<>();
     }
 
     @Override
-    public void load(OfflinePlayer player) {
-
+    public void getPrefix(OfflinePlayer offlinePlayer, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerPrefix(offlinePlayer.getUniqueId());
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
     }
 
     @Override
-    public void unload(OfflinePlayer player) {
-
+    public void getPrefix(OfflinePlayer offlinePlayer, String worldname, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerPrefix(offlinePlayer.getUniqueId());
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
     }
 
     @Override
-    public boolean isLoaded(OfflinePlayer arg0) {
-        return true;
+    public void getPrefix(OfflinePlayer offlinePlayer, String worldname, String ladder, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerPrefix(offlinePlayer.getUniqueId(), ladder);
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
+    }
+
+    @Override
+    public void getSuffix(OfflinePlayer offlinePlayer, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerSuffix(offlinePlayer.getUniqueId());
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
+    }
+
+    @Override
+    public void getSuffix(OfflinePlayer offlinePlayer, String worldname, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerSuffix(offlinePlayer.getUniqueId());
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
+    }
+
+    @Override
+    public void getSuffix(OfflinePlayer offlinePlayer, String worldname, String ladder, ImporterCallback<String> importerCallback) {
+        ListenableFuture<String> second = permissionManager.getPlayerPrefix(offlinePlayer.getUniqueId(), ladder);
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
+    }
+
+    @Override
+    public void hasPermission(OfflinePlayer offlinePlayer, String permission, ImporterCallback<Boolean> importerCallback) {
+        ListenableFuture<Boolean> second = permissionManager.playerHasPermission(offlinePlayer.getUniqueId(), permission, "", "");
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
+    }
+
+    @Override
+    public void hasPermission(OfflinePlayer offlinePlayer, String permission, String worldname, ImporterCallback<Boolean> importerCallback) {
+        ListenableFuture<Boolean> second = permissionManager.playerHasPermission(offlinePlayer.getUniqueId(), permission, worldname, "");
+        try {
+            importerCallback.call(second.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            importerCallback.call(null);
+        }
     }
 }
